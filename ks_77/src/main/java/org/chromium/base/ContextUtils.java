432a23732d9b27c44cec938a9d7d3f96327d6021
sifacaii <sifacai@outlook.com>
2025-05-17 12:05:36 +0800

    fix

--- ks_77/src/main/java/org/chromium/base/ContextUtils.java
+++ ks_77/src/main/java/org/chromium/base/ContextUtils.java
@@ -1,3 +1,7 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package org.chromium.base;
 
 import android.app.Application;
@@ -7,99 +11,154 @@ import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.os.Process;
 import android.preference.PreferenceManager;
+
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.MainDex;
 
+/**
+ * This class provides Android application context related utility methods.
+ */
 @JNINamespace("base::android")
-/* loaded from: xwalk_main_fat-77.3.aar:classes.jar:org/chromium/base/ContextUtils.class */
 public class ContextUtils {
     private static final String TAG = "ContextUtils";
     private static Context sApplicationContext;
+    // TODO(agrieve): Remove sProcessName caching when we stop supporting JB.
     private static String sProcessName;
-    static final /* synthetic */ boolean $assertionsDisabled;
 
-    static {
-        $assertionsDisabled = !ContextUtils.class.desiredAssertionStatus();
-    }
-
-    static /* synthetic */ SharedPreferences access$000() {
-        return fetchAppSharedPreferences();
-    }
-
-    /* loaded from: xwalk_main_fat-77.3.aar:classes.jar:org/chromium/base/ContextUtils$Holder.class */
+    /**
+     * Initialization-on-demand holder. This exists for thread-safe lazy initialization.
+     */
     private static class Holder {
-        private static SharedPreferences sSharedPreferences = ContextUtils.access$000();
-
-        private Holder() {
-        }
+        // Not final for tests.
+        private static SharedPreferences sSharedPreferences = fetchAppSharedPreferences();
     }
 
+    /**
+     * Get the Android application context.
+     *
+     * Under normal circumstances there is only one application context in a process, so it's safe
+     * to treat this as a global. In WebView it's possible for more than one app using WebView to be
+     * running in a single process, but this mechanism is rarely used and this is not the only
+     * problem in that scenario, so we don't currently forbid using it as a global.
+     *
+     * Do not downcast the context returned by this method to Application (or any subclass). It may
+     * not be an Application object; it may be wrapped in a ContextWrapper. The only assumption you
+     * may make is that it is a Context whose lifetime is the same as the lifetime of the process.
+     */
     public static Context getApplicationContext() {
         return sApplicationContext;
     }
 
-    @MainDex
+    /**
+     * Initializes the java application context.
+     *
+     * This should be called exactly once early on during startup, before native is loaded and
+     * before any other clients make use of the application context through this class.
+     *
+     * @param appContext The application context.
+     */
+    @MainDex // TODO(agrieve): Could add to whole class if not for ApplicationStatus.initialize().
     public static void initApplicationContext(Context appContext) {
-        if (!$assertionsDisabled && sApplicationContext != null && sApplicationContext != appContext && ((ContextWrapper) sApplicationContext).getBaseContext() != appContext) {
-            throw new AssertionError();
-        }
+        // Conceding that occasionally in tests, native is loaded before the browser process is
+        // started, in which case the browser process re-sets the application context.
+        assert sApplicationContext == null || sApplicationContext == appContext
+                || ((ContextWrapper) sApplicationContext).getBaseContext() == appContext;
         initJavaSideApplicationContext(appContext);
     }
 
+    /**
+     * Only called by the static holder class and tests.
+     *
+     * @return The application-wide shared preferences.
+     */
     private static SharedPreferences fetchAppSharedPreferences() {
         return PreferenceManager.getDefaultSharedPreferences(sApplicationContext);
     }
 
+    /**
+     * This is used to ensure that we always use the application context to fetch the default shared
+     * preferences. This avoids needless I/O for android N and above. It also makes it clear that
+     * the app-wide shared preference is desired, rather than the potentially context-specific one.
+     *
+     * @return application-wide shared preferences.
+     */
     public static SharedPreferences getAppSharedPreferences() {
         return Holder.sSharedPreferences;
     }
 
+    /**
+     * Occasionally tests cannot ensure the application context doesn't change between tests (junit)
+     * and sometimes specific tests has its own special needs, initApplicationContext should be used
+     * as much as possible, but this method can be used to override it.
+     *
+     * @param appContext The new application context.
+     */
     @VisibleForTesting
     public static void initApplicationContextForTests(Context appContext) {
         initJavaSideApplicationContext(appContext);
-        SharedPreferences unused = Holder.sSharedPreferences = fetchAppSharedPreferences();
+        Holder.sSharedPreferences = fetchAppSharedPreferences();
     }
 
     private static void initJavaSideApplicationContext(Context appContext) {
-        if (!$assertionsDisabled && appContext == null) {
-            throw new AssertionError();
-        }
-        if (BuildConfig.DCHECK_IS_ON && (appContext instanceof Application)) {
+        assert appContext != null;
+        // Guard against anyone trying to downcast.
+        if (BuildConfig.DCHECK_IS_ON && appContext instanceof Application) {
             appContext = new ContextWrapper(appContext);
         }
         sApplicationContext = appContext;
     }
 
+    /**
+     * In most cases, {@link Context#getAssets()} can be used directly. Modified resources are
+     * used downstream and are set up on application startup, and this method provides access to
+     * regular assets before that initialization is complete.
+     *
+     * This method should ONLY be used for accessing files within the assets folder.
+     *
+     * @return Application assets.
+     */
     public static AssetManager getApplicationAssets() {
-        Context applicationContext = getApplicationContext();
-        while (true) {
-            Context context = applicationContext;
-            if (context instanceof ContextWrapper) {
-                applicationContext = ((ContextWrapper) context).getBaseContext();
-            } else {
-                return context.getAssets();
-            }
+        Context context = getApplicationContext();
+        while (context instanceof ContextWrapper) {
+            context = ((ContextWrapper) context).getBaseContext();
         }
+        return context.getAssets();
     }
 
+    /**
+     * @return Whether the process is isolated.
+     */
     public static boolean isIsolatedProcess() {
         try {
-            return ((Boolean) Process.class.getMethod("isIsolated", new Class[0]).invoke(null, new Object[0])).booleanValue();
-        } catch (Exception e) {
+            return (Boolean) Process.class.getMethod("isIsolated").invoke(null);
+        } catch (Exception e) { // No multi-catch below API level 19 for reflection exceptions.
+            // If fallback logic is ever needed, refer to:
+            // https://chromium-review.googlesource.com/c/chromium/src/+/905563/1
             throw new RuntimeException(e);
         }
     }
 
+    /** @return The name of the current process. E.g. "org.chromium.chrome:privileged_process0". */
     public static String getProcessName() {
+        // Once we drop support JB, this method can be simplified to not cache sProcessName and call
+        // ActivityThread.currentProcessName().
         if (sProcessName != null) {
             return sProcessName;
         }
         try {
+            // An even more convenient ActivityThread.currentProcessName() exists, but was not added
+            // until JB MR2.
             Class<?> activityThreadClazz = Class.forName("android.app.ActivityThread");
-            Object activityThread = activityThreadClazz.getMethod("currentActivityThread", new Class[0]).invoke(null, new Object[0]);
-            sProcessName = (String) activityThreadClazz.getMethod("getProcessName", new Class[0]).invoke(activityThread, new Object[0]);
+            Object activityThread =
+                    activityThreadClazz.getMethod("currentActivityThread").invoke(null);
+            // Before JB MR2, currentActivityThread() returns null when called on a non-UI thread.
+            // Cache the name to allow other threads to access it.
+            sProcessName =
+                    (String) activityThreadClazz.getMethod("getProcessName").invoke(activityThread);
             return sProcessName;
-        } catch (Exception e) {
+        } catch (Exception e) { // No multi-catch below API level 19 for reflection exceptions.
+            // If fallback logic is ever needed, refer to:
+            // https://chromium-review.googlesource.com/c/chromium/src/+/905563/1
             throw new RuntimeException(e);
         }
     }
